0c6ab409dc546441a75224de8b87ec4dd9a8a238
Poojitha368 <poojithareddy2662004@gmail.com>
2025-08-05 22:14:43 +0530

    otp connection

--- otp-based-login/src/App.jsx
+++ otp-based-login/src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useRef } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function OTP({ otpLength = 6 }) {
+  const [otpFields, setOtpFields] = useState(new Array(otpLength).fill(""));
+  const ref = useRef([]);
+  function handleKeyDown(e, index) {
+    const key = e.key; // This tells us which keyboard key has been pressed
+    console.log(key, e);
+    // Make a copt of the current array
+    const copyOtpFields = [...otpFields];
+
+    // Let's check if the key is backspace
+    // and delete the digit in that scenario
+    if (key === "Backspace") {
+      copyOtpFields[index] = "";
+      setOtpFields(copyOtpFields);
+    }
+
+    // We should allow single digit entry only
+    if (!/^\d$/.test(key)) {
+      // We check if the pressed key is not a number
+      // we simply return from this function
+      // as only numbers are allowed!
+      return;
+    }
+
+    copyOtpFields[index] = key;
+    setOtpFields(copyOtpFields);
+
+    // use ref to focus on the next input box when an input is done
+    if (index + 1 < otpFields.length) ref.current[index + 1].focus();
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container">
+      {otpFields.map((value, index) => (
+        <input
+          key={index}
+          ref={(currentInput) => (ref.current[index] = currentInput)}
+          value={value}
+          type="text"
+          maxLength={1}
+          className="otp-input"
+          onKeyDown={(e) => handleKeyDown(e, index)}
+        />
+      ))}
+    </div>
+  );
 }
-
-export default App
